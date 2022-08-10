7184ffce491db20c303c7f456114825632dc3d69
cheks123 <chekwubeutomi@gmail.com>
2022-08-10 07:33:06 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function Home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EdMobile</h1>
+      <p>
+        EdMobile is geared towards providing study materials
+        for students in the senior secondary school and for teachers like.
+      </p>
+      <h2>Available Subjects</h2>
+      <p>We have the following subjects:</p>
+      <ul>
+        <li>Mathematics</li>
+        <li>English Language</li>
+        <li>Physics</li>
+        <li>Chemistry</li>
+        <li>Biology</li>
+        <li>Economics</li>
+        <li>Commerce</li>
+        <li>Government</li>
+        <li>Civic Education</li>
+      </ul>
+      <h2>Supported Examinations</h2>
+      <p>We support the following examinations:</p>
+      <ul>
+        <li>WAEC</li>
+        <li>NECO</li>
+        <li>JAMB</li>
+      </ul>
+      <h2>Other Resources</h2>
+      <p>We also have the following resources:</p>
+      <ul>
+        <li>Past questions and answers for JAMB and WAEC</li>
+        <li>Quiz section</li>
+        <li>Comprehensive lesson notes for teachers</li>
+        <li>National and international curriculum</li>
+        <li>Syllabuses for local examinations</li>
+        <li>Syllabuses for international examinations</li>
+      </ul>
+
     </div>
   );
 }
 
-export default App;
+export default Home;
