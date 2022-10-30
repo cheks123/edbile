72e67f104389459e1db15147764bf2cb06d7826e
Chekwube Utomi <59573850+cheks123@users.noreply.github.com>
2022-10-30 01:09:57 +0100

    Edited the intro to mathematics sentences

--- src/pages/mathematics/MathematicsIntro.js
+++ src/pages/mathematics/MathematicsIntro.js
@@ -3,12 +3,14 @@ function MathematicsIntro() {
     return (
       <>             
                 <h1>Mathematics</h1>
-                <p>Mathematics is an essential subject that has application in all field of life.
-                  It is one of the compulsory subjects in the Nigeria curriculum. Without passing mathematics,
+                <p>Mathematics is an essential subject that has applications in all fields of life.
+                  It is one of the compulsory subjects in the Nigerian curriculum. Without passing mathematics,
                   it is impossible for a student to gain admission into the university.Students should therefore
-                  strive to have a good understanding of the subject so as to be able to progress in their
-                  academics.
-                  We will take time to explain the following topics:
+                  put in there best in ensuring that they have a good grasp of the subject in other to be able to 
+                  make progress in their academics.
+                 </p>
+                 <p>
+                  On this platform, let us work on the following topics with you:
                 </p>
                 <ul>
                   <li>Indices</li>
